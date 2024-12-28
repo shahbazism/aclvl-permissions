e3643de9e0d1dcfaffba4e120a1bbbb959defc2a
saeidshahbazi <arash9775@gmail.com>
2024-12-28 18:40:49 +0330

    init management

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='aclvl_permissions',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(),
     install_requires=[
         'Django>=3.0',
